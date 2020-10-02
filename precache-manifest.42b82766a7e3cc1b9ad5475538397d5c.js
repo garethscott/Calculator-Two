8e7310a6dff3c7f7ae1c63ffca838b2cfa7c96d9
garethscott <59648675+garethscott@users.noreply.github.com>
2020-10-02 15:21:59 +0100

    Updates

---
+++ precache-manifest.42b82766a7e3cc1b9ad5475538397d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86d3c547b133cb1b1b474fbbcba1718",
+    "url": "/Calculator-Two/index.html"
+  },
+  {
+    "revision": "1007958880e78d535ddf",
+    "url": "/Calculator-Two/static/css/main.fba4d719.chunk.css"
+  },
+  {
+    "revision": "6dd53e676deee6220595",
+    "url": "/Calculator-Two/static/js/2.c19bc449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Calculator-Two/static/js/2.c19bc449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1007958880e78d535ddf",
+    "url": "/Calculator-Two/static/js/main.138d938b.chunk.js"
+  },
+  {
+    "revision": "32be50df259ce53fee1e",
+    "url": "/Calculator-Two/static/js/runtime-main.b3045aa6.js"
+  }
+]);
